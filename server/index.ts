06af09609ab993806ef3be18015ad0f7e80e9253
gmtdn <failgim@gmail.com>
2026-03-14 09:31:01 +0300

    new fixes for server

--- server/index.ts
+++ server/index.ts
@@ -37,4 +37,13 @@ app.use(errorMiddleware);
 
 start(); */
 
+if (!process.env.DB_URL) {
+  console.error('DB_URL is not defined in environment variables');
+} else {
+  mongoose
+    .connect(process.env.DB_URL)
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.error('MongoDB connection error:', err));
+}
+
 export default app;
